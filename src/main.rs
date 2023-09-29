000d3b6eee7d522b57de550eb0f9cd6c35750d7b
Salem Yaslem <s@sy.sa>
2023-09-30 02:46:26 +0300

    use zbus, gstreamer and pipewiresrc to record wayland

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{collections::HashMap, os::fd::RawFd};
+use gst::prelude::*;
+use gstreamer as gst;
+use std::collections::HashMap;
 use zbus::{
     dbus_proxy,
     export::futures_util::TryStreamExt,
-    zvariant::{Fd, ObjectPath, OwnedObjectPath, Value},
+    zvariant::{ObjectPath, OwnedObjectPath, Structure, Value},
     Connection, MessageStream, MessageType, Result,
 };
 
@@ -13,11 +15,6 @@ use zbus::{
 )]
 trait ScreenCast {
     async fn create_session(&self, options: HashMap<&str, Value<'_>>) -> Result<OwnedObjectPath>;
-    async fn open_pipe_wire_remote(
-        &self,
-        session_handle: ObjectPath<'_>,
-        options: HashMap<&str, Value<'_>>,
-    ) -> Result<Fd>;
     async fn select_sources(
         &self,
         session_handle: ObjectPath<'_>,
@@ -31,93 +28,104 @@ trait ScreenCast {
     ) -> Result<OwnedObjectPath>;
 }
 
+async fn handle_session(
+    screen_cast_proxy: ScreenCastProxy<'_>,
+    response: HashMap<&str, Value<'_>>,
+) -> Result<()> {
+    let response_session_handle = response
+        .get("session_handle")
+        .unwrap()
+        .clone()
+        .downcast::<String>()
+        .expect("cannot down cast");
+
+    screen_cast_proxy
+        .select_sources(
+            ObjectPath::try_from(response_session_handle.clone())?,
+            HashMap::from([("handle_token", Value::from("blue_recorder_1"))]),
+        )
+        .await?;
+
+    screen_cast_proxy
+        .start(
+            ObjectPath::try_from(response_session_handle.clone())?,
+            "parent_window",
+            HashMap::from([("handle_token", Value::from("blue_recorder_1"))]),
+        )
+        .await?;
+    Ok(())
+}
+
+async fn record_screen_cast(response: HashMap<&str, Value<'_>>) -> Result<()> {
+    let stream: &Value<'_> = response.get("streams").unwrap();
+
+    // get fields from nested structure inside elements
+    // NOTICE: this is not the best way to do this, but it works for now
+    let stream_node_id: u32 = stream
+        .clone()
+        .downcast::<Vec<Value>>()
+        .expect("cannot down cast")
+        .get(0)
+        .unwrap()
+        .clone()
+        .downcast::<Structure>()
+        .expect("cannot down cast")
+        .fields()
+        .get(0)
+        .unwrap()
+        .clone()
+        .downcast::<u32>()
+        .unwrap();
+
+    // launch gstreamer pipeline
+    let pipeline: std::result::Result<gst::Element, gst::glib::Error> = gst::parse_launch(&format!(
+            "pipewiresrc do-timestamp=true keepalive-time=1000  path={stream_node_id} ! videorate ! video/x-raw,framerate=30/1 ! videoconvert chroma-mode=none dither=none matrix-mode=output-only ! queue ! vp8enc cpu-used=16 max-quantizer=17 deadline=1 keyframe-mode=disabled threads=8 static-threshold=1000 buffer-size=20000 ! queue ! webmmux ! filesink location=test.mp4"
+        ));
+
+    // start pipeline
+    let pipeline: gst::Pipeline = pipeline
+        .expect("pipeline error")
+        .dynamic_cast::<gst::Pipeline>()
+        .expect("pipeline error");
+
+    pipeline
+        .set_state(gst::State::Playing)
+        .expect("failed to start pipeline");
+
+    println!("Recording Wayland screen cast...");
+    Ok(())
+}
+
 #[async_std::main]
 async fn main() -> Result<()> {
+    gst::init().expect("Failed to initialize GStreamer.");
     let connection = Connection::session().await?;
-    pipewire::init();
+    let screen_cast_proxy: ScreenCastProxy<'_> = ScreenCastProxy::new(&connection).await?;
 
-    let screencast_proxy = ScreenCastProxy::new(&connection).await?;
-
-    screencast_proxy
+    screen_cast_proxy
         .create_session(HashMap::from([
-            ("handle_token", Value::from("bluerecorder_1")),
-            ("session_handle_token", Value::from("bluerecorder_1")),
+            ("handle_token", Value::from("blue_recorder_1")),
+            ("session_handle_token", Value::from("blue_recorder_1")),
         ]))
         .await?;
 
     let mut stream = MessageStream::from(connection);
-    let mut response_session_handle: String = String::default();
 
     while let Some(msg) = stream.try_next().await? {
         match msg.message_type() {
             MessageType::Signal => {
-                println!("\n\nSignal message: {:?}", msg);
                 let (_, response) = msg.body::<(u32, HashMap<&str, Value>)>()?;
-                println!("\n\nSignal response: {:?}", response);
+                if response.len() == 0 {
+                    continue;
+                }
 
-                if response.len() > 0 {
-                    if response.contains_key("session_handle") {
-                        response_session_handle = response
-                            .get("session_handle")
-                            .unwrap()
-                            .clone()
-                            .downcast::<String>()
-                            .expect("cannot down cast");
-                        println!("response_session_handle: {:?}", response_session_handle);
-                        let screencast_select_sources = screencast_proxy
-                            .select_sources(
-                                ObjectPath::try_from(response_session_handle.clone())?,
-                                HashMap::from([("handle_token", Value::from("bluerecorder_1"))]),
-                            )
-                            .await?;
-                        println!("screencast_select_sources: {screencast_select_sources}");
+                if response.contains_key("session_handle") {
+                    handle_session(screen_cast_proxy.clone(), response.clone()).await?;
+                }
 
-                        let screencast_start = screencast_proxy
-                            .start(
-                                ObjectPath::try_from(response_session_handle.clone())?,
-                                "parent_window",
-                                HashMap::from([("handle_token", Value::from("bluerecorder_1"))]),
-                            )
-                            .await?;
-                        println!("screencast_start: {screencast_start}");
-                    }
-
-                    if response.contains_key("streams") {
-                        println!(
-                            "\n\nSignal response (stream): {:?}",
-                            response.get("streams").unwrap()
-                        );
-
-                        let screencast_pipe_wire = screencast_proxy
-                            .open_pipe_wire_remote(
-                                ObjectPath::try_from(response_session_handle.clone())?,
-                                HashMap::from([("handle_token", Value::from("bluerecorder_1"))]),
-                            )
-                            .await?;
-                        println!("screencast_pipe_wire: {screencast_pipe_wire}");
-
-                        let mainloop =
-                            pipewire::MainLoop::new().expect("Failed to create Pipewire Mainloop");
-                        let pipewire_context = pipewire::Context::new(&mainloop)
-                            .expect("Failed to create new pipewire context");
-                        let pipewire_core = pipewire_context
-                            .connect_fd(
-                                RawFd::from(
-                                    screencast_pipe_wire.to_string().parse::<i32>().unwrap(),
-                                ),
-                                None
-                            )
-                            .expect("Cannot connect to pipewire file descriper");
-                        let _registry_listener = pipewire_core
-                            .add_listener_local()
-                            .done(|_, _| println!("done"))
-                            .error(|_, _, _, _| println!("error"))
-                            .info(|info| println!("info {:?}", info))
-                            .register();
-                            
-                        mainloop.run();
-                        // pipewire_core.
-                    }
+                if response.contains_key("streams") {
+                    // TODO: start recording on separate thread
+                    record_screen_cast(response.clone()).await?;
                 }
             }
             MessageType::MethodReturn => {
